baab77e2f4c7568614b49687c403fdeb01c8318f
rsokolen <romeeo82@intetics.com>
2020-02-18 12:10:34 +0200

    MovieDbContext formating changed

--- MovieDatabase/Models/MovieDbContext.cs
+++ MovieDatabase/Models/MovieDbContext.cs
@@ -26,35 +26,59 @@ namespace MovieDatabase.Models
         {
             // For Alexey Beletskiy --> in my humble opinion column names on the scheme of DB are more suspicious than my properties therefore not implemented
 
-            modelBuilder.Entity<Actor>().Property(id => id.Id).IsRequired();
-            modelBuilder.Entity<Actor>().Property(fname => fname.FirstName).HasMaxLength(20);
-            modelBuilder.Entity<Actor>().Property(lname => lname.LastName).HasMaxLength(20);
-            modelBuilder.Entity<Actor>().Property(gender => gender.Gender).HasMaxLength(1);
-            modelBuilder.Entity<Actor>().HasMany<MovieCast>(actor => actor.MovieCasts).WithOne(moviecast => moviecast.Actor);
+            modelBuilder.Entity<Actor>().Property(id => id.Id)
+                    .IsRequired();
+            modelBuilder.Entity<Actor>().Property(fname => fname.FirstName)
+                    .HasMaxLength(20);
+            modelBuilder.Entity<Actor>().Property(lname => lname.LastName)
+                    .HasMaxLength(20);
+            modelBuilder.Entity<Actor>().Property(gender => gender.Gender)
+                    .HasMaxLength(1);
+            modelBuilder.Entity<Actor>().HasMany(actor => actor.MovieCasts)
+                    .WithOne(moviecast => moviecast.Actor);
 
-            modelBuilder.Entity<Movie>().Property(id => id.Id).IsRequired();
-            modelBuilder.Entity<Movie>().Property(title => title.Title).HasMaxLength(20);
-            modelBuilder.Entity<Movie>().Property(lang => lang.Language).HasMaxLength(50);
-            modelBuilder.Entity<Movie>().Property(country => country.Country).HasMaxLength(5);
-            modelBuilder.Entity<Movie>().HasMany<MovieCast>(movie => movie.MovieCasts).WithOne(moviecast => moviecast.Movie);
-            modelBuilder.Entity<Movie>().HasMany<MovieDirection>(movie => movie.MovieDirections).WithOne(moviedir => moviedir.Movie);
-            modelBuilder.Entity<Movie>().HasMany<Rating>(movie => movie.Ratings).WithOne(raiting => raiting.Movie);
-            modelBuilder.Entity<Movie>().HasMany<MovieGenre>(movie => movie.MovieGenres).WithOne(moviegenre => moviegenre.Movie);
+            modelBuilder.Entity<Movie>().Property(id => id.Id)
+                    .IsRequired();
+            modelBuilder.Entity<Movie>().Property(title => title.Title)
+                    .HasMaxLength(20);
+            modelBuilder.Entity<Movie>().Property(lang => lang.Language)
+                    .HasMaxLength(50);
+            modelBuilder.Entity<Movie>().Property(country => country.Country)
+                    .HasMaxLength(5);
+            modelBuilder.Entity<Movie>().HasMany(movie => movie.MovieCasts)
+                    .WithOne(moviecast => moviecast.Movie);
+            modelBuilder.Entity<Movie>().HasMany(movie => movie.MovieDirections)
+                    .WithOne(moviedir => moviedir.Movie);
+            modelBuilder.Entity<Movie>().HasMany(movie => movie.Ratings)
+                    .WithOne(raiting => raiting.Movie);
+            modelBuilder.Entity<Movie>().HasMany(movie => movie.MovieGenres)
+                    .WithOne(moviegenre => moviegenre.Movie);
 
-            modelBuilder.Entity<Director>().Property(id => id.Id).IsRequired();
-            modelBuilder.Entity<Director>().Property(fname => fname.FirstName).HasMaxLength(20);
-            modelBuilder.Entity<Director>().Property(lname => lname.LastName).HasMaxLength(20);
-            modelBuilder.Entity<Director>().HasMany<MovieDirection>(director => director.MovieDirections).WithOne(moviedir => moviedir.Director);
+            modelBuilder.Entity<Director>().Property(id => id.Id)
+                    .IsRequired();
+            modelBuilder.Entity<Director>().Property(fname => fname.FirstName)
+                    .HasMaxLength(20);
+            modelBuilder.Entity<Director>().Property(lname => lname.LastName)
+                    .HasMaxLength(20);
+            modelBuilder.Entity<Director>().HasMany(director => director.MovieDirections)
+                    .WithOne(moviedir => moviedir.Director);
 
-            modelBuilder.Entity<Reviewer>().Property(id => id.Id).IsRequired();
-            modelBuilder.Entity<Reviewer>().Property(name => name.Name).HasMaxLength(30);
-            modelBuilder.Entity<Reviewer>().HasMany<Rating>(reviewer => reviewer.Ratings).WithOne(raiting => raiting.Reviewer);
+            modelBuilder.Entity<Reviewer>().Property(id => id.Id)
+                    .IsRequired();
+            modelBuilder.Entity<Reviewer>().Property(name => name.Name)
+                    .HasMaxLength(30);
+            modelBuilder.Entity<Reviewer>().HasMany(reviewer => reviewer.Ratings)
+                    .WithOne(raiting => raiting.Reviewer);
 
-            modelBuilder.Entity<Genre>().Property(id => id.Id).IsRequired();
-            modelBuilder.Entity<Genre>().Property(title => title.Title).HasMaxLength(20);
-            modelBuilder.Entity<Genre>().HasMany<MovieGenre>(genre => genre.MovieGenres).WithOne(moviegenre => moviegenre.Genre);
+            modelBuilder.Entity<Genre>().Property(id => id.Id)
+                    .IsRequired();
+            modelBuilder.Entity<Genre>().Property(title => title.Title)
+                    .HasMaxLength(20);
+            modelBuilder.Entity<Genre>().HasMany(genre => genre.MovieGenres)
+                    .WithOne(moviegenre => moviegenre.Genre);
 
-            modelBuilder.Entity<MovieCast>().Property(role => role.Role).HasMaxLength(30);
+            modelBuilder.Entity<MovieCast>().Property(role => role.Role)
+                    .HasMaxLength(30);
             modelBuilder.Entity<MovieCast>().HasKey(mc => new { mc.ActorId, mc.MovieId });
 
             modelBuilder.Entity<MovieDirection>().HasKey(md => new { md.DirectorId, md.MovieId });
