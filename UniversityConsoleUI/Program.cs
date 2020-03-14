315508081030593ff47a062926052a3498f85210
rsokolen <romeeo82@intetics.com>
2020-03-14 08:53:56 +0200

    Song added :-)

--- UniversityConsoleUI/Program.cs
+++ UniversityConsoleUI/Program.cs
@@ -2,6 +2,8 @@
 using University.DAL.Repositories;
 using University.DAL;
 using University.DAL.Models;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace University.ConsoleUI
 {
@@ -24,8 +26,64 @@ namespace University.ConsoleUI
             //var students = unitOfWork.StudentRepository.Get();
             //var departments = unitOfWork.DepartmentRepository.Get();
             //var courses = unitOfWork.CourseRepository.Get();
-            
+
             MainMenuLaunch();
+
+            new Task(MissionImpossible).Start();
+        }
+
+        private static void MissionImpossible()
+        {
+            for (; ; )
+            {
+                Console.Beep(784, 150);
+                Thread.Sleep(300);
+                Console.Beep(784, 150);
+                Thread.Sleep(300);
+                Console.Beep(932, 150);
+                Thread.Sleep(150);
+                Console.Beep(1047, 150);
+                Thread.Sleep(150);
+                Console.Beep(784, 150);
+                Thread.Sleep(300);
+                Console.Beep(784, 150);
+                Thread.Sleep(300);
+                Console.Beep(699, 150);
+                Thread.Sleep(150);
+                Console.Beep(740, 150);
+                Thread.Sleep(150);
+                Console.Beep(784, 150);
+                Thread.Sleep(300);
+                Console.Beep(784, 150);
+                Thread.Sleep(300);
+                Console.Beep(932, 150);
+                Thread.Sleep(150);
+                Console.Beep(1047, 150);
+                Thread.Sleep(150);
+                Console.Beep(784, 150);
+                Thread.Sleep(300);
+                Console.Beep(784, 150);
+                Thread.Sleep(300);
+                Console.Beep(699, 150);
+                Thread.Sleep(150);
+                Console.Beep(740, 150);
+                Thread.Sleep(150);
+                Console.Beep(932, 150);
+                Console.Beep(784, 150);
+                Console.Beep(587, 1200);
+                Thread.Sleep(75);
+                Console.Beep(932, 150);
+                Console.Beep(784, 150);
+                Console.Beep(554, 1200);
+                Thread.Sleep(75);
+                Console.Beep(932, 150);
+                Console.Beep(784, 150);
+                Console.Beep(523, 1200);
+                Thread.Sleep(150);
+                Console.Beep(466, 150);
+                Console.Beep(523, 150);
+                Thread.Sleep(500);
+            }
         }
 
         private static void MainMenuLaunch()
